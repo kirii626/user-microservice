8adc0800d3ced94bd289074b36e7abc22b3e7d7b
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-30 14:02:01 -0300

    delete boilerplate by using lombok annotations

--- src/main/java/com/accenture/user_microservice/models/UserEntity.java
+++ src/main/java/com/accenture/user_microservice/models/UserEntity.java
@@ -3,7 +3,10 @@ package com.accenture.user_microservice.models;
 import com.accenture.user_microservice.models.enums.RoleType;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @Entity
 public class UserEntity {
 
@@ -11,15 +14,19 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
 
+    @Setter
     @Column(nullable = false)
     private String username;
 
+    @Setter
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Setter
     @Column(nullable = false)
     private String password;
 
+    @Setter
     @Enumerated(EnumType.STRING)
     private RoleType roleType = RoleType.USER;
 
@@ -33,49 +40,12 @@ public class UserEntity {
         this.roleType = roleType;
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
     @Override
     public String toString() {
         return "UserEntity{" +
                 "userId=" + userId +
                 ", username='" + username + '\'' +
                 ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 ", roleType=" + roleType +
                 '}';
     }
