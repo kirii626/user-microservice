053d501521cf3ca0e72c65a35ca1600205f9b33a
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-30 16:14:00 -0300

    create mapper for dtos

--- src/main/java/com/accenture/user_microservice/services/mappers/UserMapper.java
+++ src/main/java/com/accenture/user_microservice/services/mappers/UserMapper.java
@@ -1,23 +1,29 @@
 package com.accenture.user_microservice.services.mappers;
 
 import com.accenture.user_microservice.dtos.UserDtoEmailRole;
+import com.accenture.user_microservice.dtos.UserDtoIdUsernameEmail;
 import com.accenture.user_microservice.dtos.UserDtoInput;
 import com.accenture.user_microservice.dtos.UserDtoOutput;
 import com.accenture.user_microservice.models.UserEntity;
 import com.accenture.user_microservice.models.enums.RoleType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Component
 public class UserMapper {
+
+    private final PasswordEncoder passwordEncoder;
 
     public UserEntity userDtoInputToEntity(UserDtoInput userDtoInput) {
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername(userDtoInput.getUsername());
         userEntity.setEmail(userDtoInput.getEmail());
-        userEntity.setPassword(userDtoInput.getPassword());
+        userEntity.setPassword(passwordEncoder.encode(userDtoInput.getPassword()));
 
         return userEntity;
     }
@@ -37,10 +43,18 @@ public class UserMapper {
                 .collect(Collectors.toList());
     }
 
-    public static UserDtoEmailRole toUserDtoEmailRole(UserEntity userEntity) {
+    public UserDtoEmailRole toUserDtoEmailRole(UserEntity userEntity) {
         return new UserDtoEmailRole(
                 userEntity.getEmail(),
                 userEntity.getRoleType()
+        );
+    }
+
+    public UserDtoIdUsernameEmail toUserDtoIdUsernameEmail(UserEntity userEntity) {
+        return new UserDtoIdUsernameEmail(
+                userEntity.getUserId(),
+                userEntity.getUsername(),
+                userEntity.getEmail()
         );
     }
 }
