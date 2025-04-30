7d0c2cc18eaeeb6b08afd76aaac61fa626c6d29f
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-30 15:25:02 -0300

    add CustomUserDetailsService, JwtUtils and SecurityConfig to configure spring security and jwt

---
+++ src/main/java/com/accenture/user_microservice/config/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.accenture.user_microservice.config.security;
+
+import com.accenture.user_microservice.models.UserEntity;
+import com.accenture.user_microservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new User(userEntity.getEmail(), userEntity.getPassword(), AuthorityUtils.createAuthorityList(userEntity.getRoleType().toString()));
+    }
+}
--- src/main/java/com/accenture/user_microservice/repositories/UserRepository.java
+++ src/main/java/com/accenture/user_microservice/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import com.accenture.user_microservice.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    Optional<UserEntity> findByEmail(String username);
 }
