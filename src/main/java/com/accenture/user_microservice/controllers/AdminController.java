2110b8b3bbbb2d5fddb51ad58c4b1a8bc52b7237
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-04 00:22:39 -0300

    create AdminController and AuthController

--- src/main/java/com/accenture/user_microservice/controllers/AdminController.java
+++ src/main/java/com/accenture/user_microservice/controllers/AdminController.java
@@ -3,22 +3,21 @@ package com.accenture.user_microservice.controllers;
 import com.accenture.user_microservice.dtos.UserDtoEmailRole;
 import com.accenture.user_microservice.dtos.UserDtoOutput;
 import com.accenture.user_microservice.dtos.UserDtoRole;
+import com.accenture.user_microservice.dtos.UserDtoIdUsernameEmail;
 import com.accenture.user_microservice.services.UserService;
 import com.accenture.user_microservice.utils.ApiResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/admin")
-public class UserAdminController {
+public class AdminController {
 
     private final UserService userService;
-
-    public UserAdminController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/all-users")
     public ApiResponse<List<UserDtoOutput>> getAllUsers() {
@@ -29,5 +28,10 @@ public class UserAdminController {
     public ApiResponse<UserDtoEmailRole> changeRoleType(@Valid @PathVariable Long userId,
                                                         @RequestBody UserDtoRole userDtoRole) {
         return userService.changeRoleType(userId, userDtoRole);
+    }
+
+    @GetMapping("/user-by-id/{userId}")
+    public UserDtoIdUsernameEmail getUserById(@PathVariable Long userId) {
+        return userService.getUserById(userId);
     }
 }
---
+++ src/main/java/com/accenture/user_microservice/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.accenture.user_microservice.controllers;
+
+import com.accenture.user_microservice.dtos.UserDtoInput;
+import com.accenture.user_microservice.dtos.UserDtoOutput;
+import com.accenture.user_microservice.dtos.UserDtoEmailPassword;
+import com.accenture.user_microservice.services.AuthService;
+import com.accenture.user_microservice.utils.ApiResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping
+    public ApiResponse<UserDtoOutput> registerUser(@Valid @RequestBody UserDtoInput userDtoInput) {
+        return authService.createUser(userDtoInput);
+    }
+
+    @PostMapping("/log-in")
+    public ResponseEntity<String> authenticateUser(@Valid @RequestBody UserDtoEmailPassword userDtoEmailPassword) {
+        String jwt = authService.authenticateAndGenerateToken(
+                userDtoEmailPassword.getEmail(),
+                userDtoEmailPassword.getPassword());
+        return ResponseEntity.ok(jwt);
+    }
+}
