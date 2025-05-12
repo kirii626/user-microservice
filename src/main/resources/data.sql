d1acebc4955cf0937dc243ee361ed37d11be5a97
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-12 16:28:54 -0300

    delete user_id from data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,12 @@
-INSERT INTO user_entity (user_id, username, email, password, role_type) VALUES
-  (1, 'ana', 'ana@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (2, 'beto', 'beto@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (3, 'carla', 'carla@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (4, 'daniel', 'daniel@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (5, 'elena', 'elena@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (6, 'fabian', 'fabian@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'ADMIN'),
-  (7, 'gabriela', 'gabriela@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'ADMIN'),
-  (8, 'hector', 'hector@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (9, 'irene', 'irene@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
-  (10, 'julian', 'julian@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER');
+INSERT INTO user_entity (username, email, password, role_type) VALUES
+  ('ana', 'ana@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('beto', 'beto@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('carla', 'carla@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('daniel', 'daniel@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('elena', 'elena@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('fabian', 'fabian@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'ADMIN'),
+  ('gabriela', 'gabriela@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'ADMIN'),
+  ('hector', 'hector@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('irene', 'irene@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('julian', 'julian@example.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER'),
+  ('kiara', 'kiaraalejandrajustinianoolmos@gmail.com', '$2a$12$A5JgPTRLNPnL5D7vcLgEEOz90Ha6Hwf81qeGCur/GN1FGFyjndOUy', 'USER');
