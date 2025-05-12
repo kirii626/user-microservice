83385afd22161f808c15ae63ed5b6f515053f535
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-12 16:33:28 -0300

    enable and config redis cache

--- src/main/java/com/accenture/user_microservice/UserMicroserviceApplication.java
+++ src/main/java/com/accenture/user_microservice/UserMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.accenture.user_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/accenture/user_microservice/config/security/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.accenture.user_microservice.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("user::")
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+    }
+}
