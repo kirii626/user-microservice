e51a9be38ab848b223d56d9564d510b63183aa16
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-26 09:44:11 -0300

    use ActiveProfiles annotation for use the test profile variables

--- src/test/java/com/accenture/user_microservice/UserMicroserviceApplicationTests.java
+++ src/test/java/com/accenture/user_microservice/UserMicroserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.accenture.user_microservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserMicroserviceApplicationTests {
 
 	@Test
