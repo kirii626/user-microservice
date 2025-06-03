d4d210e486431b8f8a19d886751b900a6b65e14e
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-03 09:11:54 -0300

    enhance configuration of RedisConfig

--- src/main/java/com/accenture/user_microservice/config/RedisConfig.java
+++ src/main/java/com/accenture/user_microservice/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.accenture.user_microservice.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -11,12 +14,18 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
+        ObjectMapper redisObjectMapper = new ObjectMapper();
+        redisObjectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+
         return RedisCacheConfiguration.defaultCacheConfig()
-                .prefixCacheNameWith("user::")
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer()
-                        )
-                );
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
     }
+
 }
