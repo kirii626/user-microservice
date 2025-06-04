863ec212ac5204d7466de2ef2dc915b60e38835e
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-04 12:32:21 -0300

    fix: improve configurations of RedisConfig defining LettuceConnectionFactory and CacheManager

--- src/main/java/com/accenture/user_microservice/config/RedisConfig.java
+++ src/main/java/com/accenture/user_microservice/config/RedisConfig.java
@@ -3,14 +3,26 @@ package com.accenture.user_microservice.config;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        return new LettuceConnectionFactory(host, port);
+    }
 
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
@@ -26,6 +38,14 @@ public class RedisConfig {
 
         return RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+    }
+
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration())
+                .build();
     }
 
 }
